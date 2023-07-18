59c073bff894fd5d638d77f325cfa7ff6b9a4f1f
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-17 20:10:11 -0400

    Finished the shape of the JSX to render the 8x8 grid of a board.

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,38 @@ export function App() {
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ],
     id: null,
-    winner: null,
+    state: null,
+    mines: null,
   })
 
   return (
     <div>
       <h1> Minesweeper API</h1>
       <ul>
-        <li></li>
+        <li>{game.board[0][0]}</li>
+        <li>{game.board[0][1]}</li>
+        <li>{game.board[0][2]}</li>
+        <li>{game.board[0][3]}</li>
+        <li>{game.board[0][4]}</li>
+        <li>{game.board[0][5]}</li>
+        <li>{game.board[0][6]}</li>
+        <li>{game.board[0][7]}</li>
+        <li>{game.board[1][0]}</li>
+        <li>{game.board[1][1]}</li>
+        <li>{game.board[1][2]}</li>
+        <li>{game.board[1][3]}</li>
+        <li>{game.board[1][4]}</li>
+        <li>{game.board[1][5]}</li>
+        <li>{game.board[1][6]}</li>
+        <li>{game.board[1][7]}</li>
+        <li>{game.board[2][0]}</li>
+        <li>{game.board[2][1]}</li>
+        <li>{game.board[2][2]}</li>
+        <li>{game.board[2][3]}</li>
+        <li>{game.board[2][4]}</li>
+        <li>{game.board[2][5]}</li>
+        <li>{game.board[2][6]}</li>
+        <li>{game.board[2][7]}</li>
       </ul>
     </div>
   )
