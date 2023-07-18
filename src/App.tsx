7d4005d92a38c23842ac7d1b529318091e61ae07
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-17 20:00:30 -0400

    Generated a game state based on the response expected from the API new game. A grid 8x8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  // Begin by defining the state of the game grid based on the response we expect to receive from the API. 8x8 grid.
+  const [game, setGame] = useState({
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
+  return (
+    <div>
+      <h1> Minesweeper API</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  )
 }
