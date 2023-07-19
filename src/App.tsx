98352144f7e30956f6ae94bf74ca01b3050ab191
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-19 18:45:54 -0400

    Adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
   type Game = {
     board: Board
     id: null | number
-    state: 'new' | 'playing' | 'won' | 'lost'
+    state: null | 'new' | 'playing' | 'won' | 'lost'
     mines: null | number
   }
 
@@ -33,8 +33,6 @@ export function App() {
 
   // Define difficulty state
   const [difficulty, setDifficulty] = useState<0 | 1 | 2>(0)
-
-  const [game, setGame] = useState<Game | null>(null)
 
   // Step 1.2 Define a function to create a new game by fetching from the API
   async function handleNewGame(newGameDifficulty: 0 | 1 | 2) {
@@ -81,7 +79,7 @@ export function App() {
     }
 
     // Generate the URL we need
-    const url = `https://sdg-tic-tac-toe-api.herokuapp.com/games/${game.id}/${action}`
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/${action}`
 
     const fetchOptions = {
       method: 'POST',
@@ -143,17 +141,15 @@ export function App() {
 
       <p>Instructions: Select the difficulty level to begin.</p>
       <h2>
-        <button onClick={() => handleNewGame(0)}>EASY</button>
-        <button className="new-game" onClick={() => handleNewGame(1)}>
-          MEDIUM
+        <button className="new-game" onClick={() => handleNewGame(0)}>
+          EASY
         </button>
-        <button onClick={() => handleNewGame(2)}>HARD</button>
       </h2>
       <h3>
         Game ID:{game.id}, containing {game.mines} mines. Difficulty:
         {difficulty}
       </h3>
-      <ul>
+      <section className={`difficulty-${difficulty}`}>
         {game.board.map(function (gameRow, row) {
           return gameRow.map(function (square, col) {
             return (
@@ -175,73 +171,7 @@ export function App() {
             )
           })
         })}
-
-        {/* <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
-        <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
-        <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
-        <li onClick={() => handleClickCell(0, 3)}>{game.board[0][3]}</li>
-        <li onClick={() => handleClickCell(0, 4)}>{game.board[0][4]}</li>
-        <li onClick={() => handleClickCell(0, 5)}>{game.board[0][5]}</li>
-        <li onClick={() => handleClickCell(0, 6)}>{game.board[0][6]}</li>
-        <li onClick={() => handleClickCell(0, 7)}>{game.board[0][7]}</li>
-        <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
-        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
-        <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
-        <li onClick={() => handleClickCell(1, 3)}>{game.board[1][3]}</li>
-        <li onClick={() => handleClickCell(1, 4)}>{game.board[1][4]}</li>
-        <li onClick={() => handleClickCell(1, 5)}>{game.board[1][5]}</li>
-        <li onClick={() => handleClickCell(1, 6)}>{game.board[1][6]}</li>
-        <li onClick={() => handleClickCell(1, 7)}>{game.board[1][7]}</li>
-        <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
-        <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
-        <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
-        <li onClick={() => handleClickCell(2, 3)}>{game.board[2][3]}</li>
-        <li onClick={() => handleClickCell(2, 4)}>{game.board[2][4]}</li>
-        <li onClick={() => handleClickCell(2, 5)}>{game.board[2][5]}</li>
-        <li onClick={() => handleClickCell(2, 6)}>{game.board[2][6]}</li>
-        <li onClick={() => handleClickCell(2, 7)}>{game.board[2][7]}</li>
-
-        <li onClick={() => handleClickCell(2, 0)}>{game.board[3][0]}</li>
-        <li onClick={() => handleClickCell(3, 1)}>{game.board[3][1]}</li>
-        <li onClick={() => handleClickCell(3, 2)}>{game.board[3][2]}</li>
-        <li onClick={() => handleClickCell(3, 3)}>{game.board[3][3]}</li>
-        <li onClick={() => handleClickCell(3, 4)}>{game.board[3][4]}</li>
-        <li onClick={() => handleClickCell(3, 5)}>{game.board[3][5]}</li>
-        <li onClick={() => handleClickCell(3, 6)}>{game.board[3][6]}</li>
-        <li onClick={() => handleClickCell(3, 7)}>{game.board[3][7]}</li>
-        <li onClick={() => handleClickCell(4, 0)}>{game.board[4][0]}</li>
-        <li onClick={() => handleClickCell(4, 1)}>{game.board[4][1]}</li>
-        <li onClick={() => handleClickCell(4, 2)}>{game.board[4][2]}</li>
-        <li onClick={() => handleClickCell(4, 3)}>{game.board[4][3]}</li>
-        <li onClick={() => handleClickCell(4, 4)}>{game.board[4][4]}</li>
-        <li onClick={() => handleClickCell(4, 5)}>{game.board[4][5]}</li>
-        <li onClick={() => handleClickCell(4, 6)}>{game.board[4][6]}</li>
-        <li onClick={() => handleClickCell(4, 7)}>{game.board[4][7]}</li>
-        <li onClick={() => handleClickCell(5, 0)}>{game.board[5][0]}</li>
-        <li onClick={() => handleClickCell(5, 1)}>{game.board[5][1]}</li>
-        <li onClick={() => handleClickCell(5, 2)}>{game.board[5][2]}</li>
-        <li onClick={() => handleClickCell(5, 3)}>{game.board[5][3]}</li>
-        <li onClick={() => handleClickCell(5, 4)}>{game.board[5][4]}</li>
-        <li onClick={() => handleClickCell(5, 5)}>{game.board[5][5]}</li>
-        <li onClick={() => handleClickCell(5, 6)}>{game.board[5][6]}</li>
-        <li onClick={() => handleClickCell(5, 7)}>{game.board[5][7]}</li>
-        <li onClick={() => handleClickCell(6, 0)}>{game.board[6][0]}</li>
-        <li onClick={() => handleClickCell(6, 1)}>{game.board[6][1]}</li>
-        <li onClick={() => handleClickCell(6, 2)}>{game.board[6][2]}</li>
-        <li onClick={() => handleClickCell(6, 3)}>{game.board[6][3]}</li>
-        <li onClick={() => handleClickCell(6, 4)}>{game.board[6][4]}</li>
-        <li onClick={() => handleClickCell(6, 5)}>{game.board[6][5]}</li>
-        <li onClick={() => handleClickCell(6, 6)}>{game.board[6][6]}</li>
-        <li onClick={() => handleClickCell(6, 7)}>{game.board[6][7]}</li>
-        <li onClick={() => handleClickCell(7, 0)}>{game.board[7][0]}</li>
-        <li onClick={() => handleClickCell(7, 1)}>{game.board[7][1]}</li>
-        <li onClick={() => handleClickCell(7, 2)}>{game.board[7][2]}</li>
-        <li onClick={() => handleClickCell(7, 3)}>{game.board[7][3]}</li>
-        <li onClick={() => handleClickCell(7, 4)}>{game.board[7][4]}</li>
-        <li onClick={() => handleClickCell(7, 5)}>{game.board[7][5]}</li>
-        <li onClick={() => handleClickCell(7, 6)}>{game.board[7][6]}</li>
-        <li onClick={() => handleClickCell(7, 7)}>{game.board[7][7]}</li> */}
-      </ul>
+      </section>
     </div>
   )
 }
