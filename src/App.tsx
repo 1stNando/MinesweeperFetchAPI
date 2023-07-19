d2ec7ffcc94d50992a377ff4c16fa099cd117ae7
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-19 12:51:35 -0400

    Correct basic layout of 8x8 achieved

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ export function App() {
         <li onClick={() => handleClickCell(0, 6)}>{game.board[0][6]}</li>
         <li onClick={() => handleClickCell(0, 7)}>{game.board[0][7]}</li>
         <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
-        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
+        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}X</li>
         <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
         <li onClick={() => handleClickCell(1, 3)}>{game.board[1][3]}</li>
         <li onClick={() => handleClickCell(1, 4)}>{game.board[1][4]}</li>
@@ -72,6 +72,46 @@ export function App() {
         <li onClick={() => handleClickCell(2, 5)}>{game.board[2][5]}</li>
         <li onClick={() => handleClickCell(2, 6)}>{game.board[2][6]}</li>
         <li onClick={() => handleClickCell(2, 7)}>{game.board[2][7]}</li>
+        <li onClick={() => handleClickCell(2, 0)}>{game.board[3][0]}</li>
+        <li onClick={() => handleClickCell(3, 1)}>{game.board[3][1]}</li>
+        <li onClick={() => handleClickCell(3, 2)}>{game.board[3][2]}</li>
+        <li onClick={() => handleClickCell(3, 3)}>{game.board[3][3]}</li>
+        <li onClick={() => handleClickCell(3, 4)}>{game.board[3][4]}</li>
+        <li onClick={() => handleClickCell(3, 5)}>{game.board[3][5]}</li>
+        <li onClick={() => handleClickCell(3, 6)}>{game.board[3][6]}</li>
+        <li onClick={() => handleClickCell(3, 7)}>{game.board[3][7]}</li>
+        <li onClick={() => handleClickCell(4, 0)}>{game.board[4][0]}</li>
+        <li onClick={() => handleClickCell(4, 1)}>{game.board[4][1]}</li>
+        <li onClick={() => handleClickCell(4, 2)}>{game.board[4][2]}</li>
+        <li onClick={() => handleClickCell(4, 3)}>{game.board[4][3]}</li>
+        <li onClick={() => handleClickCell(4, 4)}>{game.board[4][4]}</li>
+        <li onClick={() => handleClickCell(4, 5)}>{game.board[4][5]}</li>
+        <li onClick={() => handleClickCell(4, 6)}>{game.board[4][6]}</li>
+        <li onClick={() => handleClickCell(4, 7)}>{game.board[4][7]}</li>
+        <li onClick={() => handleClickCell(5, 0)}>{game.board[5][0]}</li>
+        <li onClick={() => handleClickCell(5, 1)}>{game.board[5][1]}</li>
+        <li onClick={() => handleClickCell(5, 2)}>{game.board[5][2]}</li>
+        <li onClick={() => handleClickCell(5, 3)}>{game.board[5][3]}</li>
+        <li onClick={() => handleClickCell(5, 4)}>{game.board[5][4]}</li>
+        <li onClick={() => handleClickCell(5, 5)}>{game.board[5][5]}</li>
+        <li onClick={() => handleClickCell(5, 6)}>{game.board[5][6]}</li>
+        <li onClick={() => handleClickCell(5, 7)}>{game.board[5][7]}</li>
+        <li onClick={() => handleClickCell(6, 0)}>{game.board[6][0]}</li>
+        <li onClick={() => handleClickCell(6, 1)}>{game.board[6][1]}</li>
+        <li onClick={() => handleClickCell(6, 2)}>{game.board[6][2]}</li>
+        <li onClick={() => handleClickCell(6, 3)}>{game.board[6][3]}</li>
+        <li onClick={() => handleClickCell(6, 4)}>{game.board[6][4]}</li>
+        <li onClick={() => handleClickCell(6, 5)}>{game.board[6][5]}</li>
+        <li onClick={() => handleClickCell(6, 6)}>{game.board[6][6]}</li>
+        <li onClick={() => handleClickCell(6, 7)}>{game.board[6][7]}</li>
+        <li onClick={() => handleClickCell(7, 0)}>{game.board[7][0]}</li>
+        <li onClick={() => handleClickCell(7, 1)}>{game.board[7][1]}</li>
+        <li onClick={() => handleClickCell(7, 2)}>{game.board[7][2]}</li>
+        <li onClick={() => handleClickCell(7, 3)}>{game.board[7][3]}</li>
+        <li onClick={() => handleClickCell(7, 4)}>{game.board[7][4]}</li>
+        <li onClick={() => handleClickCell(7, 5)}>{game.board[7][5]}</li>
+        <li onClick={() => handleClickCell(7, 6)}>{game.board[7][6]}</li>
+        <li onClick={() => handleClickCell(7, 7)}>{game.board[7][7]}</li>
       </ul>
     </div>
   )
