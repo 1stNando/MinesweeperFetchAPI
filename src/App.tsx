a9ca08d898e1ad7fe4880076540c77a4c766ae4b
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-02-22 20:20:51 -0500

    Working app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 
 export function App() {
+  // Cell values:
+
+  //   (empty space) An unrevealed cell
+
+  //   _ An empty revealed cell
+
+  //   F An unrevealed flagged cell
+
+  //   * A cell with a bomb in it (only show after a game is won or lost)
+
+  //   @ A flagged cell with a bomb in it (only show after a game is won or lost)
+
+  //   1-8 The number of neighboring cells that contain a mine.
+
   // Declare types used:
   type Cell = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | '*' | 'F' | ' ' | '_' | '@'
   type Row = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell]
@@ -14,7 +28,7 @@ export function App() {
   }
 
   // Step 1: static implementation
-  //Begin by defining the state of the game grid based on the response we expect to receive from the API. 8x8 grid.
+  //Begin by defining the state of the game grid based on the response we expect to receive from the API. 8x8 grid
   const [game, setGame] = useState<Game>({
     board: [
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -33,30 +47,6 @@ export function App() {
 
   // Define difficulty state
   const [difficulty, setDifficulty] = useState<0 | 1 | 2>(0)
-
-  // Step 1.2 Define a function to create a new game by fetching from the API
-  async function handleNewGame(newGameDifficulty: 0 | 1 | 2) {
-    const gameOptions = { difficulty: newGameDifficulty }
-
-    const url = 'https://minesweeper-api.herokuapp.com/games'
-
-    const fetchOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(gameOptions),
-    }
-
-    const response = await fetch(url, fetchOptions)
-
-    // Check point
-    console.log(response)
-
-    if (response.ok) {
-      const newGame = (await response.json()) as Game
-      setGame(newGame)
-      setDifficulty(newGameDifficulty)
-    }
-  }
 
   // Step 1.1 Define a function to handleClicks
   async function handleClickCell(
@@ -132,6 +122,33 @@ export function App() {
     }
   }
 
+  // Step 1.2 Define a function to create a new game by fetching from the API
+  async function handleNewGame(newGameDifficulty: 0 | 1 | 2) {
+    const gameOptions = { difficulty: newGameDifficulty }
+
+    // Visit this page to review the API resource information.
+    const url = 'https://minesweeper-api.herokuapp.com/games'
+
+    // Explain the response we expect from the API call
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(gameOptions),
+    }
+
+    // Variable to hold the response we await
+    const response = await fetch(url, fetchOptions)
+
+    // Check point
+    console.log(response)
+
+    if (response.ok) {
+      const newGame = (await response.json()) as Game
+      setGame(newGame)
+      setDifficulty(newGameDifficulty)
+    }
+  }
+
   ////////////////////////////////////////////////////////////////////////////////
   const header = game.state ? `You have ${game.state}` : 'Minesweeper API -'
 
@@ -139,7 +156,7 @@ export function App() {
     <div>
       <h1>{header}</h1>
 
-      <p>Instructions: Select the difficulty level to begin.</p>
+      <p>Instructions: Select the difficulty level to begin!!!</p>
       <h2>
         <button className="new-game" onClick={() => handleNewGame(0)}>
           EASY
